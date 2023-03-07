4a5a61425af17b6bf53d4f6170a0f210ce67018f
Caliche Orozco <caliche2da1@gmail.com>
2023-03-06 23:37:26 -0600

    removed about

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import Hero from "./components/Hero";
-import About from "./components/About";
 
 
 function App() {
   return (
     <Routes>
     <Route path="/" element={<Hero /> } />
-    <Route path="/about" element={<About />} />
     </Routes>
   );
 }
