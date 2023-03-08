7bc4557924df18757bde8af2967b9df0055f3161
Caliche Orozco <caliche2da1@gmail.com>
2023-03-08 04:01:59 -0600

    fix hover problem in mobile

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,54 +1,68 @@
-import React, { useState } from 'react';
-
+import { useState, useEffect } from "react";
 
 function Card({ imageSrc, title, description, background, none }) {
   const [isHovering, setIsHovering] = useState(false);
-  
+  const [isMobile, setIsMobile] = useState(false);
 
-
-
-  let classBackground = "relative rounded-lg shadow-xl h-full overflow-hidden h-80 flex flex-col items-center justify-center"  
-  let classTitle = "text-2xl font-bold text-gray-800"
-  let classDescription = "mt-2 text-gray-600"
-  let classImage = "flex items-center justify-center h-16 w-16 rounded-full mx-auto my-6 "
+  let classBackground =
+    "relative rounded-lg shadow-xl h-full overflow-hidden h-80 flex flex-col items-center justify-center";
+  let classTitle = "text-2xl font-bold text-gray-800";
+  let classDescription = "mt-2 text-gray-600";
+  let classImage =
+    "flex items-center justify-center h-16 w-16 rounded-full mx-auto my-6 ";
 
   const handleMouseEnter = () => {
-    if (background) {
-      setIsHovering(true)
+    if (background && !isMobile) {
+      setIsHovering(true);
     }
   };
 
   const handleMouseLeave = () => {
-    if (background) {
-      setIsHovering(false)
+    if (background && !isMobile) {
+      setIsHovering(false);
     }
   };
 
-
   if (!background) {
-    classBackground += " bg-white"
-    classImage += "bg-black"
-    
+    classBackground += " bg-white";
+    classImage += "bg-black";
   } else {
-    classTitle += " text-white"
-    classDescription += " text-white"
-    classImage += "bg-white"
+    classTitle += " text-white";
+    classDescription += " text-white";
+    classImage += "bg-white";
   }
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 1280);
+    };
+
+    handleResize(); // para actualizar el estado cuando se carga la página
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <div className={classBackground}>
       {/* bg-white  */}
       {background && (
         <div
           className="bg-cover bg-center absolute inset-0 z-0"
-          onMouseEnter={handleMouseEnter} 
-          onMouseLeave={ handleMouseLeave}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
           style={{ backgroundImage: `url(${background})` }}
         >
-          <div className={ isHovering ? "hidden" : "bg-black opacity-70 absolute inset-0"} ></div>
+          <div
+            className={
+              isHovering ? "hidden" : "bg-black opacity-70 absolute inset-0"
+            }
+          ></div>
         </div>
       )}
       <div className="text-center px-6 py-4 relative">
-        { none ? null : (
+        {none ? null : (
           <div
             className={isHovering ? "hidden" : classImage}
             onMouseEnter={handleMouseEnter}
@@ -56,8 +70,18 @@ function Card({ imageSrc, title, description, background, none }) {
             {imageSrc}
           </div>
         )}
-        <h2 className={isHovering ? "hidden" : classTitle } onMouseEnter={handleMouseEnter} >{title}</h2>
-        <p className={isHovering ? "hidden" :classDescription} onMouseEnter={handleMouseEnter} >{description}</p>
+        <h2
+          className={isHovering ? "hidden" : classTitle}
+          onMouseEnter={handleMouseEnter}
+        >
+          {title}
+        </h2>
+        <p
+          className={isHovering ? "hidden" : classDescription}
+          onMouseEnter={handleMouseEnter}
+        >
+          {description}
+        </p>
       </div>
     </div>
   );
