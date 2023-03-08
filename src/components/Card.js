ff7c3756f1804c5b1faa5d4ea580172492a123f2
Caliche Orozco <caliche2da1@gmail.com>
2023-03-07 20:18:19 -0600

    added the none prop

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,14 @@
 import React from "react";
 
-function Card({ imageSrc, title, description,  }) {
+function Card({ imageSrc, title, description, none  }) {
   return (
     <div className="bg-white relative rounded-lg shadow-xl h-full overflow-hidden">
-      <div className={`flex items-center justify-center h-16 w-16 bg-black rounded-full mx-auto mt-6`}>
+      {
+        none ? null : 
+        <div className={`flex items-center justify-center h-16 w-16 bg-black rounded-full mx-auto mt-6`}>
         {imageSrc}
       </div>
+      }
       <div className="text-center px-6 py-4">
         <h2 className="text-2xl font-bold text-gray-800">{title}</h2>
         <p className="mt-2 text-gray-600">{description}</p>
