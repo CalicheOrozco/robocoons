845130c050aa881aade832b18789274f549b1417
Caliche Orozco <caliche2da1@gmail.com>
2023-03-08 19:11:31 -0600

    added path to investigattion

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Hero from "./components/Hero";
+import Investigation from "./components/Investigation";
 import Robot from "./components/Robot";
 import Team from "./components/Team";
 
@@ -10,6 +11,8 @@ function App() {
     <Route path="/" element={<Hero /> } />
     <Route path="/team" element={<Team /> } />
     <Route path="/robot" element={<Robot /> } />
+    <Route path="/investigation" element={<Investigation /> } />
+
     
     </Routes>
   );
