0a8a4e1afd3dd45f801e13c3569c06ebe64747c0
Caliche Orozco <caliche2da1@gmail.com>
2023-03-09 02:58:34 -0600

    added new card

--- src/components/Investigation.js
+++ src/components/Investigation.js
@@ -3,6 +3,7 @@ import Card from "./Card";
 import Navbar from "./Navbar";
 import { RiNumber1, RiNumber2, RiNumber3, RiNumber4 } from "react-icons/ri";
 import Footer from "./Footer";
+import { Link } from "react-router-dom";
 
 export default function Investigation() {
   return (
@@ -14,7 +15,7 @@ export default function Investigation() {
           <div className="col-span-1">
             <Card
               imageSrc={<RiNumber1 className="h-10 w-10 text-black" />}
-              noHover={true}
+              
               background="./investigation-1.jpg"
               title="Problemática"
               description="Las PTAR consumen mucha energía eléctrica externa de fuentes no renovables y contaminantes, lo que representa una carga económica significativa."
@@ -24,7 +25,7 @@ export default function Investigation() {
           <div className="col-span-1">
             <Card
               imageSrc={<RiNumber2 className="h-10 w-10 text-black" />}
-              noHover={true}
+              
               background="./investigation-2.jpg"
               title="Investigación"
               description="Se investigaron sitios y expertos en ecología, energía, desarrollo ambiental, investigación e innovación."
@@ -34,11 +35,22 @@ export default function Investigation() {
           <div className="col-span-1">
             <Card
               imageSrc={<RiNumber3 className="h-10 w-10 text-black" />}
-              noHover={true}
+              
               background="./investigation-3.jpg"
               title="Solución"
               description="Crear una minicentral hidroeléctrica de tipo agua fluyente que reduzca los costos por consumo de electricidad y promueva una estrategia sustentable para la PTAR y la comunidad."
             />
+          </div>
+          <div className="col-span-1">
+            <Link to="https://drive.google.com/file/d/1zHs7kh_D_7befzZAVY0BcfwB2R5L8w4C/view">
+            <Card
+              imageSrc={<RiNumber4 className="h-10 w-10 text-black" />}
+              
+              background="./investigation-4.jpg"
+              title="Desarrollo"
+              description="Mediante la convergencia de las experiencias obtenidas de la investigación, se realizó la redacción de nuestro artículo de divulgación."
+            />
+            </Link>
           </div>
         </div>
       </div>
