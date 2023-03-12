635c87c368705e1f93828b1e20690ba538046a2a
Caliche Orozco <caliche2da1@gmail.com>
2023-03-12 00:59:34 -0600

    future: component ScrollToTop

---
+++ src/components/ScrollToTop.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import { FaArrowUp } from "react-icons/fa";
+
+const ScrollToTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility);
+    };
+  }, []);
+
+  return (
+    <div className="fixed bottom-6 right-6 z-50">
+      {isVisible && (
+        <button
+          onClick={scrollToTop}
+          className="p-3 bg-green-500 text-white rounded-full shadow-lg"
+        >
+          <FaArrowUp />
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default ScrollToTop;
